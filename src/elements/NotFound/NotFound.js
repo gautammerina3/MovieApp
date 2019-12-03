2aeb62f8993dc415363ac674c4100ca51e9c3aea
gautammmerina3 <gautammmerina3@gmail.com>
2019-12-03 13:08:35 +0545

    updated router

---
+++ src/elements/NotFound/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+    return(
+        <div>
+            <h1>Whooops. this page doesn't exists.</h1>
+        </div>
+    )
+}
+
+export default NotFound;
