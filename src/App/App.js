54a9d650b20b809352ae338fa4f8ed10cd704dd0
gautammmerina3 <gautammmerina3@gmail.com>
2019-12-03 11:50:26 +0545

    Router and Links

--- src/App/App.js
+++ src/App/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Header from '../elements/Header/Header';
 import Home from '../Home/Home';
+import Movie from '../Movie/Movie';
+import NotFound from '../elements/NotFound/NotFound'
 // import logo from './logo.svg';
 import './App.css';
 
@@ -27,10 +30,16 @@ import './App.css';
 
 const App = () => {
   return(
-    <div>
-      <Header />
-      <Home />
-    </div>
+    <BrowserRouter>
+      <React.Fragment>
+        <Header />
+        <Switch>
+          <Route path="/" component={Home} exact />
+          <Route path="/:movieId" component={Movie} exact />
+          <Route component={NotFound} /> 
+        </Switch>
+      </React.Fragment>
+    </BrowserRouter>
   )
 }
 
--- src/elements/Header/Header.js
+++ src/elements/Header/Header.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Header.css';
 
 const Header = () => {
     return (
         <div className="movie-header">
             <div className="movie-header-content">
-                <img className="rmdb-logo" src="./images/reactMovie_logo.png" alt="movie-logo"
-                />
+                <Link to="/">
+                    <img className="rmdb-logo" src="./images/reactMovie_logo.png" alt="movie-logo"
+                    />
+                </Link>
                 <img className="rmdb-tmdb-logo" src="./images/tmdb_logo.png" alt="tmdb-logo" />
             </div>
         </div>
