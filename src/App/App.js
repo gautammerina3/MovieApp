9088fb49339994043bbacaf50a765302659555b6
gautammmerina3 <gautammmerina3@gmail.com>
2019-12-03 12:46:44 +0545

    added router

--- src/App/App.js
+++ src/App/App.js
@@ -1,9 +1,6 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Header from '../elements/Header/Header';
 import Home from '../Home/Home';
-import Movie from '../Movie/Movie';
-import NotFound from '../elements/NotFound/NotFound'
 // import logo from './logo.svg';
 import './App.css';
 
@@ -30,16 +27,10 @@ import './App.css';
 
 const App = () => {
   return(
-    <BrowserRouter>
-      <React.Fragment>
-        <Header />
-        <Switch>
-          <Route path="/" component={Home} exact />
-          <Route path="/:movieId" component={Movie} exact />
-          <Route component={NotFound} /> 
-        </Switch>
-      </React.Fragment>
-    </BrowserRouter>
+    <div>
+      <Header />
+      <Home />
+    </div>
   )
 }
 
